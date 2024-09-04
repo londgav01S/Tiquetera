657d1f1fea28c37970c2cb87fb985ff270969faf
londgav01S <143038512+londgav01S@users.noreply.github.com>
2024-09-03 21:32:17 -0500

    Commit inicial, creación del proyecto

---
+++ src/main/java/co/edu/uniquindio/tiqueteo/TiqueteoApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.tiqueteo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiqueteoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiqueteoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/uniquindio/tiqueteo/TiqueteoApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.tiqueteo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiqueteoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
