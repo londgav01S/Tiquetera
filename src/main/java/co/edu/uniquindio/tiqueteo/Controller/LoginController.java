1a4c1bab9e41245961ab84f3fe0df5e33e202521
JuanNorena <133806446+JuanNorena@users.noreply.github.com>
2024-10-05 15:04:47 -0500

    correo y login google
    Se agregaron las keys del login de google, de igual manera se actualizo el metodo para enviar correos

--- src/main/java/co/edu/uniquindio/tiqueteo/Controller/LoginController.java
+++ src/main/java/co/edu/uniquindio/tiqueteo/Controller/LoginController.java
@@ -1,15 +1,20 @@
 package co.edu.uniquindio.tiqueteo.Controller;
+
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.security.core.Authentication;
 
 @Controller
 public class LoginController {
 
     @GetMapping("/login")
-    public String login() {
+    public String login(Authentication authentication) {
+        if (authentication != null && authentication.isAuthenticated()) {
+            return "redirect:/dashboard";
+        }
         return "login";
     }
 
---
+++ src/main/java/co/edu/uniquindio/tiqueteo/Services/IEmailService.java
@@ -0,0 +1,5 @@
+package co.edu.uniquindio.tiqueteo.Services;
+
+public interface IEmailService {
+    public void sendEmail(String toEmail, String subject, String body);
+}
